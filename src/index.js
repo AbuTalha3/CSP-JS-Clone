bdf098344f521fb57828f37beffdc3048d89f67c
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-08-01 16:02:10 +0500

    Update files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+import './style.css';
+
+import displayMenu from './modules/displayMenu.js';
+
+const apiUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+
+document.addEventListener('DOMContentLoaded', displayMenu);
